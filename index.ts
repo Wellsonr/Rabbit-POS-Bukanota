325340969acf8bf8e1da5302b63b7967618fef03
Richo <richo.2399@gmail.com>
2024-04-15 17:06:16 +0700

    backup

--- index.ts
+++ index.ts
@@ -38,75 +38,6 @@ amqp.connect(`amqp://bukanota_admin:TXsrHHFpy95ISjvw@${mqHost}`)
   .then(conn => {
     return conn.createChannel().then(ch => {
       ch.prefetch(1);
-      /**
-       * FETCH EXCHANGE
-       */
-      // ch.assertExchange(QUEUE_NAME, 'fanout', {
-      //   durable: true
-      // }).then(() => {
-      //   return ch.assertQueue('', {
-      //     exclusive: true
-      //   })
-      // }).then((q) => {
-      //   return ch.bindQueue(q.queue, QUEUE_NAME, '').then(() => {
-      //     return ch.consume(q.queue, msg => {
-      //       const parsed = JSON.parse(msg.content.toString())
-      //       console.log(parsed);
-      //       switch (parsed.tipe) {
-      //         case TRANSAKSI:
-      //           outlet(parsed.dbname).then(() => {
-      //             console.log("FINISH")
-      //             ch.ack(msg)
-      //           }).catch(err => {
-      //             console.warn(err)
-      //             ch.reject(msg, true)
-      //           })
-      //           break;
-      //         case EMAILOUTLET:
-      //           welcomeMail(parsed.token, parsed.email, parsed.namaoutlet, parsed.fullName).then(info => {
-      //             console.log("FINISH", info);
-      //             ch.ack(msg)
-      //           }).catch(err => {
-      //             console.warn(err)
-      //             ch.reject(msg, true)
-      //           })
-      //           break;
-      //         case HAPUSOUTLET:
-      //           deleteMail(parsed.email, parsed.namaoutlet, parsed.fullName).then(info => {
-      //             console.log("FINISH", info);
-      //             ch.ack(msg)
-      //           }).catch(err => {
-      //             console.warn(err)
-      //             ch.reject(msg, true)
-      //           })
-      //           break;
-      //         case PROSESSETTLEMENT:
-      //           prosesSettlement(parsed.nosettlement, parsed.idOutlet, parsed.tanggal, parsed.MIDReader, parsed.TIDReader).then(() => {
-      //             ch.ack(msg);
-      //           }).catch(err => {
-      //             console.warn(err)
-      //             ch.reject(msg, true)
-      //           })
-      //           break;
-      //         case RECALCULATE:
-      //           idtrans(parsed.kodeoutlet, parsed.idtrans).then(() => {
-      //             ch.ack(msg)
-      //           }).catch(err => {
-      //             console.warn(err)
-      //             ch.reject(msg, true)
-      //           })
-      //           break;
-      //         default: ch.ack(msg);
-      //       }
-      //     }, { noAck: false })
-      //   })
-      // }).then(() => {
-      //   console.log('* Waiting for messages. Ctrl+C to exit')
-      // })
-
-      /**
-       * BY CHANNEL
-       */
       const ok = ch.assertQueue(QUEUE_NAME, { durable: true })
       return ok.then(() => {
         return ch.consume(QUEUE_NAME, msg => {
@@ -126,25 +57,9 @@ amqp.connect(`amqp://bukanota_admin:TXsrHHFpy95ISjvw@${mqHost}`)
               console.log("FUNCTION DEPRECATED")
               ch.ack(msg)
               break;
-              // welcomeMail(parsed.token, parsed.email, parsed.namaoutlet, parsed.fullName).then(info => {
-              //   console.log("FINISH", info);
-              //   ch.ack(msg)
-              // }).catch(err => {
-              //   console.warn(err)
-              //   ch.reject(msg, true)
-              // })
-              break;
             case HAPUSOUTLET:
               console.log("FUNCTION DEPRECATED")
               ch.ack(msg);
-              break;
-              // deleteMail(parsed.email, parsed.namaoutlet, parsed.fullName).then(info => {
-              //   console.log("FINISH", info);
-              //   ch.ack(msg)
-              // }).catch(err => {
-              //   console.warn(err)
-              //   ch.reject(msg, true)
-              // })
               break;
             case PROSESSETTLEMENT:
               prosesSettlement(parsed.nosettlement, parsed.idOutlet, parsed.tanggal, parsed.MIDReader, parsed.TIDReader).then(() => {
